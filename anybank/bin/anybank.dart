24d2a809bfc3875283d9053676c1a69f629caf56
Pereira <pereira@eb.mil.br>
2026-02-24 15:15:40 -0300

    #2 implementações de herança

--- anybank/bin/anybank.dart
+++ anybank/bin/anybank.dart
@@ -4,6 +4,8 @@ void main() {
 
 Conta contaMatheus = Conta("Matheus",1000 );
 Conta contaRoberta = Conta("Roberta",2000 );
+ContaCorrente contaChris = ContaCorrente("Chris", 4000);
+ContaPoupanca contaDenise = ContaPoupanca("Denise", 4000);
 
 List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
@@ -17,6 +19,15 @@ conta.imprimeSaldo();
 contaRoberta.receber(3000);
 contaMatheus.receber(500);
 contaMatheus.enviar(200);
+
+contaChris.imprimeSaldo();
+contaChris.enviar(4300);
+
+contaDenise.imprimeSaldo();
+contaDenise.enviar(4300);
+contaDenise.calculaRendimento();
+contaDenise.imprimeSaldo();
+
 
 }
 
