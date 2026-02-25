6f765c002ece5ff43786bc524c836f772e38b164
Pereira <pereira@eb.mil.br>
2026-02-25 10:20:32 -0300

    #4 modificação para classe abstrata

--- anybank/bin/anybank.dart
+++ anybank/bin/anybank.dart
@@ -2,24 +2,9 @@ import 'package:anybank/conta.dart';
 
 void main() {
 
-Conta contaMatheus = Conta("Matheus",1000 );
-Conta contaRoberta = Conta("Roberta",2000 );
 ContaCorrente contaChris = ContaCorrente("Chris", 4000);
 ContaPoupanca contaDenise = ContaPoupanca("Denise", 4000);
 ContaSalario contaCatarina = ContaSalario("Catarina", 10000,"123456789", "Anybank");
-
-List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
-
-
-
-for (Conta conta in contas){
-conta.imprimeSaldo();
-}
-
-
-contaRoberta.receber(3000);
-contaMatheus.receber(500);
-contaMatheus.enviar(200);
 
 contaChris.imprimeSaldo();
 contaChris.enviar(4300);
