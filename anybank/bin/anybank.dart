60306899a301db6796b9b6d0cc3472e6aab20ca8
Pereira <pereira@eb.mil.br>
2026-02-26 10:22:26 -0300

    #5 mixin adicionado para taxa de transferência e conta investimento/empresa

--- anybank/bin/anybank.dart
+++ anybank/bin/anybank.dart
@@ -5,6 +5,8 @@ void main() {
 ContaCorrente contaChris = ContaCorrente("Chris", 4000);
 ContaPoupanca contaDenise = ContaPoupanca("Denise", 4000);
 ContaSalario contaCatarina = ContaSalario("Catarina", 10000,"123456789", "Anybank");
+ContaEmpresa contaMatheus =  ContaEmpresa("Matheus", 2000);
+ContaInvestimento contaRoberta = ContaInvestimento("Roberta", 2000 );
 
 contaChris.imprimeSaldo();
 contaChris.enviar(4300);
@@ -16,6 +18,10 @@ contaDenise.imprimeSaldo();
 
 contaCatarina.depositarSalario(5000);
 contaCatarina.imprimeSaldo();
+
+contaMatheus.enviar(1000);
+contaRoberta.receber(1000);
+
 
 
 }
--- anybank/lib/conta.dart
+++ anybank/lib/conta.dart
@@ -59,3 +59,49 @@ class ContaSalario extends Conta {
   }
 
 }
+
+mixin Imposto {
+  double taxa = 0.03;
+
+  double valorTaxado(double valor){
+    return valor*taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto {
+  ContaEmpresa(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor){
+    if (_saldo >= valor + valorTaxado(valor) ) {
+      _saldo -= valor + valorTaxado(valor);
+      imprimeSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor){
+    _saldo += valor - valorTaxado(valor);
+    imprimeSaldo();
+  }
+}
+
+class ContaInvestimento extends Conta with Imposto{
+  ContaInvestimento(super.titular, super.saldo);
+
+  @override
+  void enviar(double valor){
+    if(_saldo >= valor + valorTaxado(valor)){
+      _saldo -= valor + valorTaxado(valor);
+      imprimeSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor){
+    _saldo += valor - valorTaxado(valor);
+    imprimeSaldo();
+  }
+
+}
+
